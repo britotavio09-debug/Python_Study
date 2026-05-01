b3b742c088cb32675195bec5101665a0b57c5cfa
britotavio09-debug <britotavio09@gmail.com>
2026-04-30 22:02:46 -0300

    Primeiro commit dos estudos de Python

---
+++ EstruturaSimples.py
@@ -0,0 +1,9 @@
+A = input("Informe um valor pra a variável B: ")
+B = input("Informe um valor pra a variável B:")
+
+if(A>B):
+    aux=A;
+    A=B;
+    B=aux;
+print("O valor da variável A agora é de: ", A);
+print("O valor da variável B agora é de: ", B);
---
+++ Tipo Variavel.py
@@ -0,0 +1,7 @@
+codigo = 10
+salario = 1500.00
+nome = "José"
+situacao = True
+
+print("Codigo"+ str(codigo)+ "Nome"+ str(nome)+ "O Salário atual é de"+ str(salario))
+
